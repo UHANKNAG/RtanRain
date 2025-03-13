2fd137e951f4e3d9af46db12d504a016bdfcd69d
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2025-03-14 00:01:50 +0900

    Add random variable

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -7,7 +7,10 @@ public class Rain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = Random.Range(-2.4f, 2.4f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        transform.position = new Vector3(x, y, 0);
     }
 
     // Update is called once per frame
@@ -18,7 +21,7 @@ public class Rain : MonoBehaviour
 
     // OnCollisionEnter2D
     // On 어떤 현상이 일어났을 때, OnCollision 충돌 현상이 일어났을 때
-    // Enter 충돌이 딱 시작되는 그 순간, 2D 환경경에서
+    // Enter 충돌이 딱 시작되는 그 순간, 2D 환경에서
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 충돌하게 되면 충돌한 gameObject의 정보가 collision 안에 들어가게 됨
